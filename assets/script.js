d79ff93f54dc7562d379d054ec20b8cc611b6913
Jay Chen <93561259+JayThinks2099@users.noreply.github.com>
2021-12-01 18:57:25 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,78 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "Jay Chen"
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Gingerbread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
+let total = 0
+let qtyGb = document.getElementById("qty-gb")
+let qtyCc = document.getElementById("qty-cc")
+let qtySugar = document.getElementById("qty-sugar")
+let qtyTotal = document.getElementById("qty-total")
+let gbAdd = document.getElementById("add-gb")
+let gbMinus = document.getElementById("minus-gb")
+let ccAdd = document.getElementById("add-cc")
+let ccMinus = document.getElementById("minus-cc")
+let sugarAdd = document.getElementById("add-sugar")
+let sugarMinus = document.getElementById("minus-sugar")
+
 
 // Code to update name display 
 document.getElementById('credit').textContent = `Created by ${yourName}`
 
 // Event listener for clicks on the "+" button for Gingerbread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Gingerbread + button was clicked!')
-
-    // TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbAdd.addEventListener('click', function(e) {
+    gb = gb + 1;
+    total = gb + cc + sugar;
+    qtyGb.textContent = gb;
+    qtyTotal.textContent = total;
+    localStorage.setItem('gbCount', gb);
+    localStorage.setItem('totalCount', total);
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+gbMinus.addEventListener('click', function(e) {
+    gb = gb - 1;
+    total = gb + cc + sugar;
+    qtyGb.textContent = gb
+    qtyTotal.textContent = total;
+    localStorage.setItem('gbCount', gb);
+    localStorage.setItem('totalCount', total);
+})
+
+ccAdd.addEventListener('click', function(e) {
+    cc = cc + 1;
+    total = gb + cc + sugar;
+    qtyCc.textContent = cc;
+    qtyTotal.textContent = total;
+    localStorage.setItem('ccCount', cc);
+    localStorage.setItem('totalCount', total);
+    
+})
+
+ccMinus.addEventListener('click', function(e) {
+    cc = cc - 1;
+    total = gb + cc + sugar;
+    qtyCc.textContent = cc;
+    qtyTotal.textContent = total;
+    localStorage.setItem('ccCount', cc);
+    localStorage.setItem('totalCount', total);
+})
+
+sugarAdd.addEventListener('click', function(e) {
+    sugar = sugar + 1;
+    total = gb + cc + sugar;
+    qtySugar.textContent = sugar;
+    qtyTotal.textContent = total;
+    localStorage.setItem('sugarCount', sugar);
+    localStorage.setItem('totalCount', total);
+})
+
+sugarMinus.addEventListener('click', function(e) {
+    sugar = sugar - 1;
+    total = gb + cc + sugar;
+    qtySugar.textContent = sugar;
+    qtyTotal.textContent = total;
+    localStorage.setItem('sugarCount', sugar);
+    localStorage.setItem('totalCount', total);
+})
